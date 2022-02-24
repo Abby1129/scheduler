c7d0deb90ac08ee1a7aaab8a14935fb1a8085545
Abby F <abby_faigao@yahoo.com>
2022-02-24 14:07:02 -0800

    Build <Header> and <Empty> component, and update stories to use said components

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,13 +1,15 @@
 import React from "react";
 
+import Header from "./Header";
+
 import "components/Appointment/styles.scss";
 
 export default function Appointment(props) {
   return (
     <article className="appointment" data-testid="appointment">
       <header className="appointment__header">
-        <h3 className="text--regular">{props.student}</h3>
-        <span className="text--light">{props.time}</span>
+        <h3 className="text--regular">No appointments</h3>
+        <h3 className="text--light">{props.time}</h3>
       </header>
     </article>
   );
