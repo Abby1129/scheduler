acf32177365f5cb7ae5dc9e2cc1eaa1f2c00a4a3
Abby F <abby_faigao@yahoo.com>
2022-03-01 14:17:00 -0800

    Make helpers directory, make function to return array of appointments and test it to pass

---
+++ src/helpers/selectors.js
@@ -0,0 +1,15 @@
+export function getAppointmentsForDay(state, day) {
+  const resultArr = [];
+  for (const value of state.days) {
+    if (value.name === day) {
+      for (const appointment of value.appointments) {
+        console.log(appointment);
+        console.log(state.appointments[appointment]);
+        if (state.appointments[appointment]) {
+          resultArr.push(state.appointments[appointment]);
+        }
+      }
+    }
+  }
+  return resultArr;
+}
