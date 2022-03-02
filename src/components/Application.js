45294d13e5ab304bbf1c9298f99b05dd3c8fa7bc
Abby F <abby_faigao@yahoo.com>
2022-03-01 16:19:08 -0800

    Update state, refactor components

--- src/components/Application.js
+++ src/components/Application.js
@@ -65,9 +65,18 @@ const appointments = {
 };
 
 export default function Application(props) {
-  const [day, setDay] = useState("Monday");
-  const [days, setDays] = useState([]);
+  // const [day, setDay] = useState("Monday");
+  // const [days, setDays] = useState([]);
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {},
+  });
   const appointmentObj = Object.values(appointments);
+
+  const setDay = (day) => setState({ ...state, day });
+
+  const setDays = (days) => setState((prev) => ({ ...prev, days }));
 
   useEffect(() => {
     axios
@@ -90,7 +99,7 @@ export default function Application(props) {
         />
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
-          <DayList days={days} value={day} onChange={setDay} />
+          <DayList days={state.days} day={state.day} setDay={setDay} />
         </nav>
         <img
           className="sidebar__lhl sidebar--centered"
