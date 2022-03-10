0e28d378a92c9a6e0643bc9d4c3db221cb6172be
Abby F <abby_faigao@yahoo.com>
2022-03-10 11:47:27 -0800

    Add error feature

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -6,6 +6,7 @@ import Show from "./Show";
 import Form from "./Form";
 import Status from "./Status";
 import Confirm from "./Confirm";
+import Error from "./Error";
 
 import { useVisualMode } from "hooks/useVisualMode";
 
@@ -21,6 +22,8 @@ export default function Appointment(props) {
   const DELETING = "DELETING";
   const CONFIRM = "CONFIRM";
   const EDIT = "EDIT";
+  const ERROR_SAVE = "ERROR_SAVE";
+  const ERROR_DELETE = "ERROR_DELETE";
 
   const { mode, transition, back } = useVisualMode(
     props.interview ? SHOW : EMPTY
@@ -31,17 +34,32 @@ export default function Appointment(props) {
       student: name,
       interviewer,
     };
+
     transition(SAVING);
-    props.bookInterview(props.id, interview).then(() => {
-      transition(SHOW);
-    });
+
+    props
+      .bookInterview(props.id, interview)
+      .then(() => {
+        transition(SHOW);
+      })
+      .catch((error) => {
+        console.log(error);
+        transition(ERROR_SAVE, true);
+      });
   };
 
   const deleteAppointment = () => {
-    transition(DELETING);
-    props.cancelInterview(props.id).then(() => {
-      transition(EMPTY);
-    });
+    transition(DELETING, true);
+
+    props
+      .cancelInterview(props.id)
+      .then(() => {
+        transition(EMPTY);
+      })
+      .catch((error) => {
+        console.log(error);
+        transition(ERROR_DELETE, true);
+      });
   };
 
   return (
@@ -76,6 +94,12 @@ export default function Appointment(props) {
           onCancel={back}
           onConfirm={deleteAppointment}
         />
+      )}
+      {mode === ERROR_SAVE && (
+        <Error message="Could not save due to an error" onClose={back} />
+      )}
+      {mode === ERROR_DELETE && (
+        <Error message="Could not delete due to an error" onClose={back} />
       )}
     </article>
   );
